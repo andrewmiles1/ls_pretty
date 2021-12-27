9a7d4c0b9d7713cc64099e602849276828a8a0c5
Andrew Miles <andrew1024miles@gmail.com>
2021-12-27 18:10:43 +0000

    Fixed a left over error on display line

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@
 int main(int argc, char *argv[]) {
   std::experimental::filesystem::path cwd = 
   std::experimental::filesystem::current_path();
-
+  
 
   std::cout << "Name: \t\t\tCreated:" << std::endl;
   std::cout << "----------------------------------------" << std::endl;
 
   for(auto const& dir_entry: std::experimental::filesystem::directory_iterator{cwd} ){
-    std::cout << dir_entry.path().filename() << "\t\t\t" << dir_entry.path().std::endl;
+    std::cout << dir_entry.path().filename() << "\t\t\t" << dir_entry.path() << std::endl;
   }
 
 
