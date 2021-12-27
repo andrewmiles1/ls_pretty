f80367af87177a238a9d05cbe443a8cf19bb76b5
andrewmiles1 <andrew1024miles@gmail.com>
2021-12-27 22:56:12 +0000

    Cleaned code with 3rd part and made a base version!

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include "filesystem.hpp"
+namespace fs = ghc::filesystem;
+
 //g++ -o main main.cpp -lstdc++fs
 int main(int argc, char *argv[]) {
-  std::experimental::filesystem::path cwd = 
-  std::experimental::filesystem::current_path();
+  fs::path cwd = 
+  fs::current_path();
 
   std::cout << "Files in " << cwd << std::endl;
   std::cout << "----------------------------------------" << std::endl;
 
-  for(auto const& dir_entry: std::experimental::filesystem::directory_iterator{cwd} ){
-  	std::cout << dir_entry.path().filename() << std::endl;
-	std::string filename = dir_entry.path().filename() << std::endl;
+  for(auto const& dir_entry: fs::directory_iterator{cwd} ){
+	std::string filename = dir_entry.path().filename();
 	std::cout << filename << std::endl;
   }
 
